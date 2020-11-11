da3731cd14ba9c04274a08828d3500ae38a014c2
richmartins <richard.martinstenorio@gmail.com>
2020-11-11 22:16:32 +0100

    [wip] routing again and again

--- routes/quote.route.js
+++ routes/quote.route.js
@@ -5,12 +5,26 @@ let {check} = require('express-validator');
 let quotesController = require('../controllers/quotesController');
 
 router.get('/', (req, res) => {
+    // call controllerrrrrrrrrrrrrrrrrrrrrrrrrrrr
+    quotesController(req)
     res.json({message: "No quotes were found"});
 })
 
+router.post('/', (req, res) => {
+    res.json({message: "blablu"});
+})
+
+router.patch('/:id', [check('id').isMongoId()], (req, res) => {
+    res.json({message: "blablu"});
+});
 
 router.get('/:id', [check('id').isMongoId()], (req, res) => {
     res.json({message: "No quotes were found"});
 })
+
+router.delete('/:id', [check('id').isMongoId()], (req, res) => {
+    res.json({message: "No quotes were found"});
+})
+
 
 module.exports = router;
